806d96b446c010722fc433651125b572df3e665a
Alireza67 <Alirezaabbasim@gmail.com>
2024-03-17 00:34:52 +0330

    [WIP] memory_order_relaxed

--- cpp-features/atomics.cpp
+++ cpp-features/atomics.cpp
@@ -67,3 +67,42 @@ TEST(atomics, spinloack)
 
 	EXPECT_EQ(target, counter);
 }
+
+std::atomic<int> z;
+std::atomic<bool> x, y;
+
+void Write_x_then_y()
+{
+	x.store(true, std::memory_order_relaxed);
+	y.store(true, std::memory_order_relaxed);
+}
+
+void Read_y_then_x()
+{
+	while (!y.load(std::memory_order_relaxed));
+	if (x.load(std::memory_order_relaxed))
+	{
+		++z;
+	}
+}
+
+TEST(atomics, memory_order_relaxed)
+{
+	size_t counter{};
+	while(counter < 10'000)
+	{
+		x = false;
+		y = false;
+		z = 0;
+
+		std::thread a(Write_x_then_y);
+		std::thread b(Read_y_then_x);
+		a.join();
+		b.join();
+		if(!z.load())
+		{
+			FAIL() << "Counter: " << counter << '\n';
+		}
+		counter++;
+	}
+}
