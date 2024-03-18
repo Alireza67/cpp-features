92b850fd81db1de2bdd866179890c0d734289042
Alireza67 <Alirezaabbasim@gmail.com>
2024-03-19 00:15:14 +0330

    [UPD] queue_with_semaphore

--- test-features-20/semaphores.cpp
+++ test-features-20/semaphores.cpp
@@ -2,6 +2,9 @@
 #include <semaphore>
 #include <stop_token>
 #include <thread>
+#include <queue>
+#include <mutex>
+#include <future>
 
 using namespace std::literals;
 TEST(semaphore, binary)
@@ -13,8 +16,6 @@ TEST(semaphore, binary)
 	testSemaphore.release();
 	EXPECT_TRUE(testSemaphore.try_acquire());
 }
-
-
 
 std::binary_semaphore gloabSemaphore{ 1 };
 
@@ -49,3 +50,63 @@ TEST(semaphore, counting)
 	EXPECT_TRUE(testSemaphore.try_acquire());
 	EXPECT_TRUE(testSemaphore.try_acquire());
 }
+
+template<typename T>
+class QueueWithSemaphore
+{
+public:
+	QueueWithSemaphore(T initValue)
+		:semaphore_(initValue) {}
+
+	void Push(T& element)
+	{
+		{
+			std::lock_guard<std::mutex> lk(lock_);
+			queue_.push(element);
+		}
+		semaphore_.release();
+	}
+
+	T Pop()
+	{
+		semaphore_.acquire();
+		std::lock_guard<std::mutex> lk(lock_);
+		auto tmp = std::move(queue_.front());
+		queue_.pop();
+		return tmp;
+	}
+
+private:
+	std::mutex lock_;
+	std::queue<T> queue_{};
+	std::counting_semaphore<> semaphore_;
+};
+
+template<typename T>
+void Generator(QueueWithSemaphore<T>& queue, T data)
+{
+	queue.Push(data);
+}
+
+template<typename T>
+T Reader(QueueWithSemaphore<T>& queue)
+{
+	return queue.Pop();
+}
+
+TEST(semaphore, queue_with_semaphore)
+{
+	auto target{ 10 };
+	QueueWithSemaphore<int> queue(0);
+	auto generator = std::async(std::launch::deferred, Generator<int>, std::ref(queue), 10);
+	auto reader = std::async(std::launch::async, Reader<int>, std::ref(queue));
+	auto result = reader.wait_for(100ms);
+	EXPECT_EQ(std::future_status::timeout, result);
+	result = reader.wait_for(100ms);
+	EXPECT_EQ(std::future_status::timeout, result);
+	generator.get();
+	result = reader.wait_for(100ms);
+	EXPECT_EQ(std::future_status::ready, result);
+	EXPECT_EQ(target, reader.get());
+}
+
