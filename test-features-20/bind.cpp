30ba3425cec78234b811e51d29e16c1b11292941
Alireza67 <Alirezaabbasim@gmail.com>
2024-03-20 23:57:43 +0330

    [UPD0] std::getenv

--- test-features-20/bind.cpp
+++ test-features-20/bind.cpp
@@ -12,3 +12,15 @@ TEST(bind, bind_front)
 	auto CustomPrint = std::bind_front(PrintGreeting, "Iran"s);
 	EXPECT_EQ("Hello John. Welcome to Iran!", CustomPrint("John"));
 }
+
+TEST(getenv_, simple)
+{
+	auto env_var_name = "PATH";
+	if (auto res = std::getenv(env_var_name);
+		res != nullptr)
+	{
+		SUCCEED();
+		return;
+	}
+	FAIL();
+}
