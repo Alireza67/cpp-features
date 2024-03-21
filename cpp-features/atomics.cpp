4fd1eaac69f4fe5906bf08395186eb168066f818
Alireza67 <Alirezaabbasim@gmail.com>
2024-03-21 14:33:19 +0330

    [UPD] compare_exchange_weak

--- cpp-features/atomics.cpp
+++ cpp-features/atomics.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include <atomic>
 #include <mutex>
+#include <future>
 
 class SpinLock
 {
@@ -123,5 +124,92 @@ TEST(atomics, memory_order)
 	std::thread t1(producer);
 	std::thread t2(consumer);
 	t1.join(); t2.join();
+}
+
+TEST(atomics, boolain)
+{
+	std::atomic<bool> flag{ true };
+	EXPECT_FALSE(std::is_copy_constructible<decltype(flag)>::value);
+	EXPECT_FALSE(std::is_copy_assignable<decltype(flag)>::value);
+}
+
+/*
+	compare_exchange_weak() is one of compare-exchange primitives provided in C++11.
+	It's weak in the sense that it returns false even if the value of the object is 
+	equal to expected. This is due to spurious failure on some platforms where a 
+	sequence of instructions (instead of one as on x86) are used to implement it. 
+	On such platforms, context switch, reloading of the same address (or cache line) 
+	by another thread, etc can fail the primitive. It's spurious as it's not the value
+	of the object (not equal to expected) that fails the operation. 
+	Instead, it's kind of timing issues.
+*/
+
+template<typename T>
+void ChangeValue(std::atomic<T>& data, T value)
+{
+	data = value;
+}
+
+TEST(atomics, compare_exchange_weak)
+{
+	{
+		std::atomic<bool> data{ false };
+		auto expected = data.load();
+
+		//Do some caluculation based on old data (expected)
+		auto calcResult = true;
+
+		auto result = data.compare_exchange_weak(expected, calcResult);
+		EXPECT_TRUE(result);
+		EXPECT_FALSE(expected);
+		EXPECT_TRUE(data.load());
+	}
+	{
+		std::atomic<bool> data{ false };
+		auto expected = data.load();
+		
+		// Other Thread Change Value of data
+		auto future = std::async(
+			std::launch::async, ChangeValue<bool>, std::ref(data), true);
+		future.get();
+
+		//Do some caluculation based on old data (expected)
+		auto calcResult = true;
+
+		auto result = data.compare_exchange_weak(expected, calcResult);
+		EXPECT_FALSE(result);
+		EXPECT_TRUE(expected);
+		EXPECT_TRUE(data.load());
+	}
+	{
+		std::atomic<int> data{ 2 };
+		auto expected = data.load();
+
+		//Do some caluculation based on old data (expected)
+		auto calcResult = std::pow(expected, 2);
+
+		auto result = data.compare_exchange_weak(expected, calcResult);
+		EXPECT_TRUE(result);
+		EXPECT_EQ(2, expected);
+		EXPECT_EQ(calcResult, data.load());
+	}
+	{
+		std::atomic<int> data{ 50 };
+		auto expected = data.load();
+
+		// Other Thread Change Value of data
+		auto newValue = 65;
+		auto future = std::async(
+			std::launch::async, ChangeValue<int>, std::ref(data), newValue);
+		future.get();
+
+		//Do some caluculation based on old data (expected)
+		auto calcResult = std::pow(expected, 2);
+
+		auto result = data.compare_exchange_weak(expected, calcResult);
+		EXPECT_FALSE(result);
+		EXPECT_EQ(newValue, expected);
+		EXPECT_EQ(newValue, data.load());
+	}
 }
 
