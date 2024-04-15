3f16cfd421da9b06906c179cb268389807bb1a3a
Alireza67 <alirezaabbasim@gmail.com>
2024-04-15 22:54:06 +0330

    [ADD] non-type arguments: reference type

--- cpp-features/templates.cpp
+++ cpp-features/templates.cpp
@@ -11,3 +11,28 @@ TEST(NonType, integral)
 {
 	auto res = ArrayGenerator<int, 100>();
 }
+
+struct Department
+{
+	int departmentCode;
+	std::size_t numberOfEmployee;
+	std::string name;
+};
+
+template<Department& part>
+class Employee
+{
+public:
+	std::string Print()
+	{
+		return part.name + " Department has "s + std::to_string(part.numberOfEmployee)
+			+ " employees and it's code is "s + std::to_string(part.departmentCode);
+	}
+};
+
+TEST(NonType, reference)
+{
+	static Department math{ -5, 250, "math"s};
+	Employee<math> obj;
+	EXPECT_EQ("math Department has 250 employees and it's code is -5"s, obj.Print());
+}
